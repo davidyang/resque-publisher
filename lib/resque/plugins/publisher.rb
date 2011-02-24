1d6fea225baedc2080753cbc304f3d853f9cd372
David Yang <david.g.yang@gmail.com>
2011-02-24 18:21:05 -0500

    add logging

--- lib/resque/plugins/publisher.rb
+++ lib/resque/plugins/publisher.rb
@@ -9,6 +9,8 @@ module Resque
 
       def after_enqueue_publish(*args)
         Resque.redis.publish :publisher, { :event => :enqueued,
+          :pid => job_pid,
+          :host => hostname,
                                            :queue => @queue,
                                            :id => self.object_id,
                                            :args => args }.to_json
@@ -17,6 +19,8 @@ module Resque
       def before_perform_publish(*args)
         Resque.redis.publish :publisher, { :event => :started,
                                            :queue => @queue,
+          :pid => job_pid,
+          :host => hostname,
                                            :id => self.object_id,
                                            :args => args }.to_json
       end
@@ -24,6 +28,8 @@ module Resque
       def after_perform_publish(*args)
         Resque.redis.publish :publisher, { :event => :finished,
                                            :queue => @queue,
+          :pid => job_pid,
+          :host => hostname,
                                            :id => self.object_id,
                                            :args => args }.to_json
       end
@@ -32,8 +38,27 @@ module Resque
         Resque.redis.publish :publisher, { :event => :failed,
                                            :queue => @queue,
                                            :id => self.object_id,
+          :pid => job_pid,
+          :host => hostname,
                                            :message => message,
                                            :args => args }.to_json
+      end
+
+      def log_publish(message)
+        Resque.redis.publish :publisher, { :event => :log,
+          :queue => @queue,
+          :id => self.object_id,
+          :host => hostname,
+          :pid => job_pid,
+          :message => message }.to_json
+
+      end
+
+      def job_pid
+        $$.to_s
+      end
+      def hostname
+        `hostname`.chomp
       end
     end
   end
